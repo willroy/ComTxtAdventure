e704b46d9c95cababd2c0469cdcaaeb0ee1443ce
willroy <willjoroy@gmail.com>
2017-08-22 12:49:14 +0100

    load and save commands made and load at start loads saved state from save command

--- txtadven.rb
+++ txtadven.rb
@@ -3,21 +3,17 @@
 require 'yaml'
 
 class Character
-    attr_accessor :charinfo
-    def initialize(charinfo)
-        @charinfo = charinfo #character yaml file
-    end
     def change_name
         puts "Character name? => " #input text
         @name = gets.chomp #get input
 
-        @charinfo[:name] = @name #Modify
-        File.open('characters.yaml', 'w') {|f| f.write @charinfo.to_yaml } 
+        $charinfo[:name] = @name #Modify
+        File.open('characters.yaml', 'w') {|f| f.write $charinfo.to_yaml } 
         puts "Hello #{@name}" #show to player the name
     end
     def inventory
         puts "Inventory:"
-        @charinfo.each do |key, value|
+        $charinfo.each do |key, value|
             if key.to_s() == "items"
                 value.each do |k, v|
                     puts "#{k}"
@@ -28,7 +24,7 @@ class Character
     def examine
         puts "Which Item? "
         item = gets.chomp
-        @charinfo.each do |key, value|
+        $charinfo.each do |key, value|
             if key.to_s() == "items"
                 value.each do |k, v|
                     if k = item
@@ -41,16 +37,12 @@ class Character
 end
 
 class Room
-    def initialize(general_info, roominfo)
-        @roominfo = roominfo
-        @general_info = general_info
-    end
     def move(dir)
         goingto = ""
         breakk = false
         cango = "maybe"
-        @roominfo.each do |key, value|
-            if key == @general_info["current_room"]
+        $roominfo.each do |key, value|
+            if key == $general_info["current_room"]
                 value[:in?] = false
                 value[:exits].each do |k, v|
                     if v == dir 
@@ -61,15 +53,13 @@ class Room
                     end
                     cango = "false"
                 end
-                
-
                 breakk = true
-                @roominfo.each do |k, v|
+                $roominfo.each do |k, v|
                     v.each do |ke, val|
                         if ke.to_s() == "name" 
                             if val.to_s() == goingto.to_s()
                                 v[:in?] = true
-                                @general_info["current_room"] = k
+                                $general_info["current_room"] = k
                                 File.open('general_info.yaml', 'w') {|f| f.write @general_info.to_yaml } 
                             end
                         end
@@ -81,8 +71,9 @@ class Room
         puts "You cannot go in this direction" if cango == "false"
     end
     def room_in_desc
-        @roominfo.each do |key, value|
-            if key == @general_info["current_room"]
+        roominfo = $roominfo
+        roominfo.each do |key, value|
+            if key == $general_info["current_room"]
                 puts "You are in the #{value[:name]}"
                 value[:items].each do |k, v|
                     puts "Items in this room: #{k}"
@@ -103,37 +94,38 @@ class Game
         @character
         @room
         @quit = false
+        $roominfo
+        $general_info
+        $charinfo
+        @savegame
     end
     def init_game
         puts "New Game or Load Game? (NEW / LOAD) => "
         gametype = gets.chomp
-        if gametype.upcase == "NEW" #creates new info for the yml for new game
-            charinfo = YAML::load_file('defaultchar.yaml')
-            general_info = YAML::load_file('generaldefault.yaml')
-            roominfo = YAML::load_file('roomdefault.yaml')
-            @character = Character.new(charinfo)
+        if gametype.upcase == "NEW" #loads new info for the yml for new game
+            $charinfo = YAML::load_file('defaultchar.yaml')
+            $general_info = YAML::load_file('generaldefault.yaml')
+            $roominfo = YAML::load_file('roomdefault.yaml')
+            @character = Character.new
             @character.change_name()
-            @room = Room.new(general_info, roominfo)
+            @room = Room.new
             true
-        elsif gametype.upcase == "LOAD" #does the same thing as NEW but puts past data by loading
-            begin
-                charinfo = YAML::load(File.open('characters.yaml')) 
-            rescue ArgumentError => e #just in case
-                  puts "Could not parse CHARACTER YAML: #{e.message}"
+        elsif gametype.upcase == "LOAD" #does the same thing as NEW but different file
+            @savegame = YAML::load(File.open('savegame.yaml'))
+            count = 1
+            @savegame.each do |key, value|
+                #puts key
+                if count == 1
+                    $charinfo = key 
+                elsif count == 2
+                    $roominfo = key
+                elsif count == 3
+                    $general_info = key
+                end
+                count += 1
             end
-            @character = Character.new(charinfo)
-
-            begin 
-                roominfo = YAML::load(File.open('roominfo.yaml'))
-            rescue ArgumentError => e
-                puts "Could not parse ROOM YAML: #{e.message}"
-            end
-            begin 
-                general_info = YAML::load(File.open('general_info.yaml'))
-            rescue ArgumentError => e
-                puts "Could not parse GENERAL YAML: #{e.message}"
-            end
-            @room = Room.new(general_info, roominfo)
+            @character = Character.new
+            @room = Room.new
             false
         else
             puts ""
@@ -170,8 +162,31 @@ class Game
         @character.examine() if command == "EXAMINE" or command == "EXAM"
         @quit = true if command == "QUIT" 
     end
-    def save_game()
-        
+    def save_game
+        data = $charinfo, $roominfo, $general_info
+        File.open('savegame.yaml', 'w') {|f| f.write data.to_yaml } 
+        @savegame = YAML::load(File.open('savegame.yaml'))
+        puts "SAVED GAME!"
+        #@savegame.each do |key, value|
+        #    puts "#{key} #{value}"
+        #    puts ""
+        #end
+    end
+    def load_game 
+        puts "\nYou travel back to your last save...\n\n"
+        @savegame = YAML::load(File.open('savegame.yaml'))
+        count = 1
+        @savegame.each do |key, value|
+            #puts key
+            if count == 1
+                $charinfo = key 
+            elsif count == 2
+                $roominfo = key
+            elsif count == 3
+                $general_info = key
+            end
+            count += 1
+        end
     end
     def tutorial
         puts "\n\n To control your game you have to use commands to navigate"
@@ -182,7 +197,7 @@ class Game
     end
     def start_game
         puts "You wake up in an empty room"
-        puts "#{@character.charinfo['name']} => Where am I?"
+        puts "#{$charinfo['name']} => Where am I?"
         puts "You see a door to the north of the room"
         puts "Would you like a tutorial? (Y/n) "
         tut = gets.chomp
@@ -202,7 +217,6 @@ option = game.init_game()
 if option == true
     game.start_game 
 elsif option == false
-    puts ""
-    puts "You wake up back where you were..."
+    puts "\nYou wake up back where you were...\n\n"
     game.game_loop
 end
