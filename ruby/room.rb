9db34e9eedb27986b0328e2ed5ec6bf2a893e41e
William Roy <willjoroy@gmail.com>
2017-10-29 10:45:35 +0000

    yeah

---
+++ ruby/room.rb
@@ -0,0 +1,50 @@
+class Room
+  def move(dir)
+    goingto = ""
+    cango = "maybe"
+    $roominfo[$general_info["current_room"]][:in?] = false
+    $roominfo[$general_info["current_room"]][:exits].each do |k, v|
+      if v == dir 
+        goingto = k
+        cango = true
+        break
+      end
+      cango = false
+    end
+    $roominfo.each do |k, v|
+      v.each do |ke, val|
+        if ke.to_s() == "name" 
+          if val.to_s() == goingto.to_s()
+            v[:in?] = true
+            $general_info["current_room"] = k
+          end
+        end
+      end
+    end
+    $text.draw("You cannot go in this direction") if cango == false
+    $text.draw("You go #{dir}") if cango == true
+  end
+  def room_in_desc
+    $text.reset_pos()
+    $text.draw("You are in the #{$roominfo[$general_info["current_room"]][:name]}")
+    $text.draw("Items in room: ")
+    if $roominfo[$general_info["current_room"]]["items"] != nil
+      $roominfo[$general_info["current_room"]]["items"].each do |k| 
+        $text.draw(k)
+      end
+    end
+    $roominfo[$general_info["current_room"]][:exits].each {|k, v| $text.draw("There is the #{k} to the #{v}")}
+    $text.reset_pos()
+  end
+  def pickup(item)
+    items = $roominfo[$general_info["current_room"]]["items"]
+    exists = items.include? item
+    if exists
+      items.delete item
+    end
+    exists
+  end
+  def putinroom(item)
+    $roominfo[$general_info["current_room"]]["items"] << item
+  end
+end 
